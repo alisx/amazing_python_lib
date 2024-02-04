c04831bc97c694d58af995765125b3449823c19d
alisx <finddly@163.com>
2024-02-04 17:49:37 +0800

    art

---
+++ art/main.py
@@ -0,0 +1,37 @@
+from art import *
+
+# ==== 使用  art  来表达 ====
+tprint("Hello")
+
+tprint("Hello", font='bulbhead')
+
+# 获取所有支持的图案列表
+print(ART_NAMES)
+
+# 好玩的图形
+aprint("happy birthday 1")
+
+aprint("playing cards hearts waterfall")
+
+# 查看所有图形
+import art
+print(art.art_list())
+
+# 新年特别款
+tprint("Happy New Year", font="basic")
+
+tprint("Happy 2024!", font="block")
+
+# 随机艺术
+tprint("New Year", font="rand")
+
+# 实践练习
+'''
+- 尝试一下 `art` 其他艺术字
+- 创造并发送个性化的节日祝福
+- 给小朋友（也可以是男或女朋友）展示一下编程的乐趣
+'''
+
+
+
+
